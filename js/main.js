499bbd2a2e643a7936a018ebb1908c7ae607f33f
Your Name <pasha.rodoslavov@gmail.com>
2018-05-23 22:57:06 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// //Adds video button features
+// $('video').mediaelementplayer({
+//   features:['playpause','current','progress','volume','fullscreen']
+// });
+
+ $('video').mediaelementplayer({
+        features: ['playpause','current', 'progress', 'duration','volume', 'fullscreen'],
+      });
