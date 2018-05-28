f751008574afc021a3d0fe5d50d69198e0b1c49a
Your Name <pasha.rodoslavov@gmail.com>
2018-05-27 17:21:15 -0700

    project is done

--- js/main.js
+++ js/main.js
@@ -4,108 +4,30 @@
         features: ['playpause','current', 'progress', 'duration','volume', 'fullscreen'],
       });
 
-
  // DOM variables
  let myVideo = document.getElementById('myVideo');
  let text = document.querySelectorAll('.text');
+ let warpperText = document.getElementById('warpperText');
 
-
+// Clicking play triggers the transcript
 
 myVideo.addEventListener("timeupdate", () => {
-
-
-
-	for (let i = 0; i < text.length; i++){
+	
+	for (let i = 0; i < text.length; i++){				
 		if(myVideo.currentTime > text[i].getAttribute('data-start') && myVideo.currentTime <= text[i].getAttribute('data-end')){
-			text[i].style.color = "red";
-		}	
-		else if(text[i].onclick){
-			alert('opa');
+			text[i].classList.add("orangeColorText");
 		}
 		else {
-			text[i].style.color = "black";
+			text[i].classList.remove("orangeColorText");
 		}
 	}
 });
 
+//Clicking subtitle triggers video in necessary time
 
-
-
-
-for (let i = 0; i < text.length; i++){
-	text[i].addEventListener('click',(e) => {
-		 myVideo.currentTime = e.target.getAttribute('data-start');		 
-	})
-	// text[i].addEventListener('mouseover',(e) => {
-	// 	 	text[i].style.backgrounColor = 'green;
-	// 	};
-}
-
-
-// let warpperText = document.getElementById('warpperText');
-// 			warpperText.addEventListener('mouseover', (e) =>{
-// 				if (e.target.tagName.toUpperCase() == "SPAN"){
-// 					// console.log(e.target);
-// 					e.target.style.color = 'green';
-// 				}		
-// 			});
-	
-		
-
-
-
-// let warpperText = document.getElementById('warpperText');
-// 	warpperText.addEventListener('mouseover', (e) =>{
-// 		if (e.target.tagName.toUpperCase() == "SPAN"){
-// 			// console.log(e.target);
-// 			e.target.style.color = 'green';
-// 		}		
-// 	});
-
-
-
-// let playback = document.getElementById('playback');
-
-// 	playback.addEventListener('mouseover',(e) => {
-// 			 	// playback.style.color = 'green';
-// 			 	// console.log(myVideo.currentTime);
-// 			});
-
-
-
-
-
-// let pos = document.getElementById("demo");
-// myVideo.addEventListener( "timeupdate" , () => {pos.innerHTML = myVideo.currentTime});
-
-// $myVideo.on("timeupdate", () => {pos.innerHTML = myVideo.currentTime});
-
-
-
- // $('video').on('click', function(){
-		// console.log(myVideo.currentTime);
-	// });
-
-
-// console.log($video[0].currentTime);
-
-//  var dt = new Date();
-// var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-
-	// $('wrapperText').on('click', function(){
-	// 	console.log($video[0].currentTime);
-	// });
-
-
-
-// 	myVideo.addEventListener("timeupdate", () => {	
-// 	console.log(myVideo.currentTime);
-// 	for (let i = 0; i < text.length; i++){
-// 		if(myVideo.currentTime > text[i].getAttribute('data-start') && myVideo.currentTime <= text[i].getAttribute('data-end')){
-// 			text[i].style.color = "red";
-// 		}	
-// 		else {
-// 			text[i].style.color = "black";
-// 		}
-// 	}
-// });
+warpperText.addEventListener('click',(e) => {
+	if (e.target.tagName == ('SPAN')){
+		myVideo.currentTime = +e.target.getAttribute('data-start') + .1; // after adding 0.1sec only one subtitle is orange		 		 
+		myVideo.play();	
+	}		 
+});
